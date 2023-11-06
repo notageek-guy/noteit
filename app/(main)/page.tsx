33941b0cc71e0a889142debb4a309d1aaec9bb9f
ManishKumar <manishkumar97430@gmail.com>
2023-11-06 07:18:17 +0530

    attemp-2 to fix layout error

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -8,8 +8,15 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-200 px-8 pt-20">
-      <div>
-        <img src="/hero.svg" alt="Hero Image" />
+      <div className="text-center">
+        <img
+          src="/hero.svg"
+          alt="Hero Image"
+          className="w-64 h-64 mx-auto mb-6"
+        />
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-black mb-6">
+          Welcome to NoteIt, your note-taking web app
+        </h1>
       </div>
       <div className="flex flex-col items-center gap-4 ">
         {!userId ? (
